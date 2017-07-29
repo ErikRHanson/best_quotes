b197eba8e20f222ee49fc9a59a9617f4a3deb968
Erik Hanson <hoddrofnir@gmail.com>
2017-07-29 09:31:17 -0700

    Add Chapter 2

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+
+class HomeController < Rulers::Controller
+  
+  def home
+    "Welcome Home."
+  end
+end
---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,18 @@
+# best_quotes /app/controllers/quotes_controller.rb
+
+class QuotesController < Rulers::Controller
+  def exception
+    raise "It's a bad one!"
+  end
+  
+  def a_quote
+    "There is nothing either good or bad " +
+    "but thinking makes it so." +
+    "\n<pre>\n#{env}\n</pre>"
+  end
+  
+  def another_quote
+    "Anyone who stops learning is old."
+  end
+  
+end
